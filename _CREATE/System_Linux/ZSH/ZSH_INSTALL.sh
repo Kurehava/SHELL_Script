ef7f3c7d2e6b00090921440d5c6b7758647146cf
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2023-01-22 05:34:48 +0900

    Update ZSH_INSTALL.sh

--- _CREATE/System_Linux/ZSH/ZSH_INSTALL.sh
+++ _CREATE/System_Linux/ZSH/ZSH_INSTALL.sh
@@ -273,7 +273,7 @@ fi
 
 # change def shell
 echo -e "$info change sh to ZSH."
-chsh -s /bin/zsh "$(whoami)"
+chsh -s /bin/zsh
 if [ "$?" != "0" ];then
     echo -e "$warn For unknown reasons, we cannot change zsh for you."
     echo -e "$warn Please manually change zsh later to apply the changes."
