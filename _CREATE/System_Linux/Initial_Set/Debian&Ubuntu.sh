f336cd458a698381dba7978ca16d33204a59025d
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2023-04-11 09:53:36 +0900

    Update Debian&Ubuntu.sh

--- _CREATE/System_Linux/Initial_Set/Debian&Ubuntu.sh
+++ _CREATE/System_Linux/Initial_Set/Debian&Ubuntu.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y && apt autoremove -y
-apt install curl wget sudo neofetch net-tools apt-utils jq vim ssh gcc g++ -y
+apt install curl wget sudo neofetch net-tools apt-utils jq vim ssh git gcc g++ -y
 
 # needs
 apt install ufw (or iptables)
