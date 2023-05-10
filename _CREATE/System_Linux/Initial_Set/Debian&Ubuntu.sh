15449fd46c057cd4b62863371fdb3c426ea55656
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2023-05-10 11:29:18 +0900

    Update Debian&Ubuntu.sh

--- _CREATE/System_Linux/Initial_Set/Debian&Ubuntu.sh
+++ _CREATE/System_Linux/Initial_Set/Debian&Ubuntu.sh
@@ -1,5 +1,5 @@
 apt update -y && apt upgrade -y && apt autoremove -y
-apt install curl wget sudo neofetch net-tools apt-utils jq vim ssh git gcc g++ screen lsof -y
+apt install curl wget sudo neofetch net-tools apt-utils jq vim ssh git gcc g++ screen lsof bpytop -y
 
 # needs
 apt install ufw (or iptables)
